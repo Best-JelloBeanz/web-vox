114d7606c920fce3fe2820b3668eebc4783f1ef6
Best-JelloBeanz <93356778+Best-JelloBeanz@users.noreply.github.com>
2023-05-11 18:50:08 -0500

    Create canvas.js

---
+++ canvas.js
@@ -0,0 +1,12 @@
+var canvas = document.getElementById("test-canvas");
+var context = canvas.getContext('2d');
+
+// source for this code: https://riptutorial.com/html5-canvas/example/11659/detecting-mouse-position-on-the-canvas
+
+canvas.addEventListener("mousemove", function(e) { 
+    var cRect = canvas.getBoundingClientRect();
+    var canvasX = Math.round(e.clientX - cRect.left);
+    var canvasY = Math.round(e.clientY - cRect.top);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillText("X: "+canvasX+", Y: "+canvasY, 10, 20);
+});
