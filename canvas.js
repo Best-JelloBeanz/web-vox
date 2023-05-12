7d598cd74525e2e88659cec786cdb350fe28cdcb
Best-JelloBeanz <93356778+Best-JelloBeanz@users.noreply.github.com>
2023-05-11 19:11:49 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,6 +1,2 @@
-var canvas = document.getElementById("test-canvas");
-var context = canvas.getContext('2d');
-
-context.moveTo(20, 20);
-context.lineTo(280, 180);
-context.stroke();
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
