eaafea10194aca1773bbc30826da91fa1148b3ff
Best-JelloBeanz <93356778+Best-JelloBeanz@users.noreply.github.com>
2023-08-10 18:22:26 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -4,11 +4,14 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 1024
 
+const player = new Player()
 
 function animate() {
   window.requestAnimationFrame(animate)
   c.fillStyle = 'white'
   c.fillRect(0, 0, canvas.width, canvas.height)
+
+  player.draw()
 }
 
 animate()
