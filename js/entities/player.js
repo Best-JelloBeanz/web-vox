b555cfb67ac7018ef7571a29f133d9734b65aa1d
Best-JelloBeanz <93356778+Best-JelloBeanz@users.noreply.github.com>
2023-08-10 18:03:11 -0500

    Update player.js

--- js/entities/player.js
+++ js/entities/player.js
@@ -1,1 +1,14 @@
-
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 32
+    this.height = 32
+  }
+  draw() {
+    c.fillStyle = 'red'
+    c.fillRect(this.x, this.y, this.width, this.height)
+  }
+  updatePos() {
+  }
+)
