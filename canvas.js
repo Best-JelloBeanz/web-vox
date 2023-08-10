af2262d24adcf93d154dd426a50fe0152f434ad3
Best-JelloBeanz <93356778+Best-JelloBeanz@users.noreply.github.com>
2023-08-10 17:53:01 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -4,6 +4,8 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 1024
 
+const player = new Player()
+
 function draw() {
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
