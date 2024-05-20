cdf5b53cc8757a5ccf5131ab8d06131be1ec2767
Best-JelloBeanz <93356778+Best-JelloBeanz@users.noreply.github.com>
2024-05-20 09:05:25 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -4,18 +4,16 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-c.fillStyle = 'red'
-c.fillRect(0, 0, canvas.width, canvas.height)
+//c.fillStyle = 'red'
+//c.fillRect(0, 0, canvas.width, canvas.height)
 
-//const player = new Player()
-/*
+const player = new Player()
+
 function animate() {
   window.requestAnimationFrame(animate)
   c.fillStyle = 'red'
   c.fillRect(0, 0, canvas.width, canvas.height)
-
   player.draw()
 }
-
 animate()
-*/
+
